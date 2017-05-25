8cb019a4c541d9bcad41da51f3890273b01187b2
ChrisGottsacker <atcgottsacker@gmail.com>
2017-05-25 11:49:24 -0500

    Use filtered snps for test, all snps for similarity matrix

--- scripts/fastlmm_node.py
+++ scripts/fastlmm_node.py
@@ -26,12 +26,12 @@ species_chroms = {'human':23, 'mouse':20}
 
 # run fastlmmc
 def run_fastlmmc(dataset, output_dir, pheno_index, covFile=None, species='mouse', maxthreads=1, featsel=False, exclude=False, condition=None):
-    
+
     # commands from fastlmmc:
     # maxthreads
     # condition
     # exclude by position
-    
+
     # if condition:
     #     condition = '-SnpId1 %s' % condition[0]
     # else:
@@ -40,40 +40,41 @@ def run_fastlmmc(dataset, output_dir, pheno_index, covFile=None, species='mouse'
     # temporary kludge because -excludeByPosition option is slow (at least for v2.05 and v2.06)
 
     bfile = dataset
-    snp_reader = Bed(bfile)
+    filtered_snp_reader = Bed('%s.FILTERED' % bfile)
+    full_snp_reader = Bed('%s.FULL' % bfile)
 
     pheno_file = loadOnePhen('%s.pheno.txt' % dataset, i_pheno = pheno_index)
-    phenotype_name = pheno_file['header'][0] 
+    phenotype_name = pheno_file['header'][0]
 
     v = globals()
     chroms = map(str, range(1, species_chroms[species] + 1))
     v.update(locals())
-    
+
     # loop through chromosomes and run
     for i, chrom in enumerate(chroms):
-            
+
         # separate by chromosome for LOOCV
-        test_snps = snp_reader[:, snp_reader.pos[:,0] == int(chrom)]
-        mat_snps = snp_reader[:, snp_reader.pos[:,0] != int(chrom)]
+        test_snps = filtered_snp_reader[:, filtered_snp_reader.pos[:,0] == int(chrom)]
+        matrix_snps = full_snp_reader[:, full_snp_reader.pos[:,0] != int(chrom)]
 
         # run snp with covar
         if covFile:
-            df = single_snp(test_snps = test_snps, pheno = pheno_file, K0 = mat_snps, covar = covFile)
+            df = single_snp(test_snps = test_snps, pheno = pheno_file, K0 = matrix_snps, covar = covFile)
         else:
-            df = single_snp(test_snps = test_snps, pheno = pheno_file, K0 = mat_snps)
+            df = single_snp(test_snps = test_snps, pheno = pheno_file, K0 = matrix_snps)
 
         # format outputs
         out_df = df.loc[:, ['SNP', 'Chr', 'ChrPos', 'PValue', 'SnpWeight']]
         out_df.columns = ['SNP', 'CHR', 'BP', 'P', 'Beta']
-	    
+
         # save results into data frame
         if i == 0:
             final = out_df
         else:
             final = final.append(out_df)
-                
-                
-    # output to csv 
+
+
+    # output to csv
     v.update(locals())
     final.to_csv('%(output_dir)s/%(phenotype_name)s.gwas' % v, sep='\t', index=False)
 
@@ -110,12 +111,10 @@ if __name__ == '__main__':
     debug = args.debug
     dataset = args.dataset
     output_dir = root
-    pheno_index = int( args.pheno_index ) 
+    pheno_index = int( args.pheno_index )
 
     if debug:
         print('args:')
         pprint(args)
 
     run_fastlmmc(dataset, output_dir, pheno_index, covFile, species, maxthreads, featsel=featsel, exclude=exclude, condition=condition)
-
-
